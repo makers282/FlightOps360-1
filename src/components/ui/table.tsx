eb797722dc134cc0ead285d159b46fc21350cfc1
Matt Akers <M.Akers282@gmail.com>
2025-06-04 20:26:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <table>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >
    > ...
    >     <HTTPAccessFallbackBoundary notFound={undefined} forbidden={undefined} unauthorized={undefined}>
    >       <RedirectBoundary>
    >         <RedirectErrorBoundary router={{...}}>
    >           <InnerLayoutRouter url="/aircraft/..." tree={[...]} cacheNode={{lazyData:null, ...}} segmentPath={[...]}>
    >             <ClientPageRoot Component={function AircraftMaintenanceDetailPage} searchParams={{}} params={{...}}>
    >               <AircraftMaintenanceDetailPage params={Promise} searchParams={Promise}>
    >                 <div>
    >                   <PageHeader>
    >                   <Dialog>
    >                   <div>
    >                   <_c className="mt-6 shado...">
    >                     <div ref={null} className="rounded-lg...">
    >                       <_c2>
    >                       <_c8>
    >                         <div ref={null} className="p-6 pt-0">
    >                           <_c>
    >                             <div className="relative w...">
    > >                             <table ref={null} className="w-full caption-bottom text-sm">
    > >                               {" "}
    >                                 ...
    >                           ...
    >                   ...
    >             ...
    > src/components/ui/table.tsx (11:5) @ _c
    >
    >    9 | >(({ className, ...props }, ref) => (
    >   10 |   <div className="relative w-full overflow-auto">
    > > 11 |     <table
    >      |     ^
    >   12 |       ref={ref}
    >   13 |       className={cn("w-full caption-bottom text-sm", className)}
    >   14 |       {...props}
    > Call Stack
    > 17
    > Show 14 ignore-listed frame(s)
    > table
    > unknown (0:0)
    > _c
    > src/components/ui/table.tsx (11:5)
    > AircraftMaintenanceDetailPage
    > src/app/(app)/aircraft/currency/[tailNumber]/page.tsx (435:243)
    >

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,13 +6,15 @@ import { cn } from "@/lib/utils"
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...restProps }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
+      {...restProps}
+    >
+      {children}
+    </table>
   </div>
 ))
 Table.displayName = "Table"
