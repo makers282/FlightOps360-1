3f234391db2ee4f1ea6a6f0fcbac64a9b13e872c
Matt Akers <M.Akers282@gmail.com>
2025-06-30 19:21:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/components/login-form.tsx
+++ src/app/login/components/login-form.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
 import { useState, useTransition } from 'react';
